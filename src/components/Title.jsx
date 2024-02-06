dd02993e05eb939a2dbe5486d023c36afa4436eb
Wisnu Andika <wisnuandika104@gmail.com>
2024-02-06 22:42:44 +0700

    feat: upload project with sample layouting

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className='w-fit mx-auto'>
+      <h1 className='bg-green-500 text-4xl font-bold text-white text-center p-2'>Wisnu Andika's Website Portfolio</h1>
+      <p className='bg-blue-200'>hello</p>
+    </div>
+  )
+}
